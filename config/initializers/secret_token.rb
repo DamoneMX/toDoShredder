8cefc17bc55383447d514c57663c567c622d2c1b
Arie R <arie@dadabureau.com>
2013-09-06 13:46:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoShredder::Application.config.secret_token = '598d30b7b531d6e280b949fc35fcfc034705b99b6c80733f7fc568fbfa7a597f87245cd4bb3b233342edeabf12abb45da94f80a96487e51ac88a620be9995e60'
